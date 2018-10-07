4776aefe6ac2c39bd3edd8420371b2115d35a9b3
kevinkuo52 <kevinkuo15@gmail.com>
2018-10-07 05:15:43 -0400

    arguemnts

--- cluster.py
+++ cluster.py
@@ -1,7 +1,13 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import argparse
+import ast
+from sys import argv
 def getCluster(data):
+    data = map(float, data.strip('[]').split(','))
+
     X = np.array(data)
+    print(X)
     kmeans = KMeans(n_clusters=5, random_state=0).fit(X)
 
     centers = sorted(kmeans.cluster_centers_, key=lambda x: x[-1])
@@ -10,3 +16,5 @@ def getCluster(data):
 
     centers = centers[::-1]
     return centers
+if __name__ == '__main__':
+    getCluster(argv[1])
