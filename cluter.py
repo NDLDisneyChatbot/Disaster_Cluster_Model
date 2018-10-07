30a60e9805f6bfdaabca2e4ef65a287b57017938
kevinkuo52 <kevinkuo15@gmail.com>
2018-10-07 04:22:13 -0400

    list sort

--- cluter.py
+++ cluter.py
@@ -3,6 +3,10 @@ import numpy as np
 def getCluster(data):
     X = np.array(data)
     kmeans = KMeans(n_clusters=5, random_state=0).fit(X)
-    print(kmeans.labels_)
-    print(kmeans.cluster_centers_)
-    return kmeans.cluster_centers_
+
+    centers = sorted(kmeans.cluster_centers_, key=lambda x: x[-1])
+    for index, center in enumerate(centers):
+        centers[index] = center.tolist()
+
+    centers = centers[::-1]
+    return centers
